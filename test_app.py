aa656a0d2d6ca2cdbbc2925e663dcbc9671b6f7a
MertGurcuoglu <mrthkk23@gmail.com>
2025-06-30 18:30:54 +0300

    turkce karakter uyumsuzlugu ıcın decode eklendı

--- test_app.py
+++ test_app.py
@@ -3,5 +3,6 @@ from app import app
 def test_index():
     tester = app.test_client()
     response = tester.get('/')
+    content=response.data.decode('utf-8')
     assert response.status_code == 200
     assert b"Merhaba ben Mert Gürcüoğlu" in response.data
