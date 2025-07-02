e93360de7846767a8245a194098bc0e15faed165
MertGurcuoglu <mrthkk23@gmail.com>
2025-07-02 11:48:52 +0300

    postgreSQl baglantısı retry olacak sekılde eklendı

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-import os
+import time
+import psycopg2
 from flask import Flask
 
 app = Flask(__name__)
 
+# Retry ile veritabanı bağlantısı
+while True:
+    try:
+        conn = psycopg2.connect(
+            dbname="devdb",
+            user="devuser",
+            password="devpass",
+            host="postgres",
+            port="5432"
+        )
+        cursor = conn.cursor()
+        break  # bağlantı başarılıysa döngüden çık
+    except psycopg2.OperationalError:
+        print("PostgreSQL henüz hazır değil. 2 saniye sonra tekrar denenecek...")
+        time.sleep(2)
+
 @app.route('/')
-def home():
-    return '<h1>Merhaba ben Mert Gürcüoğlu ve bu bir DevOps projesidir.</h1>'
+def index():
+    cursor.execute("CREATE TABLE IF NOT EXISTS visits (count INT);")
+    cursor.execute("SELECT count FROM visits;")
+    result = cursor.fetchone()
+    if result:
+        count = result[0] + 1
+        cursor.execute("UPDATE visits SET count = %s;", (count,))
+    else:
+        count = 1
+        cursor.execute("INSERT INTO visits (count) VALUES (1);")
+    conn.commit()
+    return f"<h1>Bu sayfa {count} kez ziyaret edildi.</h1>"
 
 if __name__ == '__main__':
-    port = int(os.getenv("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
+
