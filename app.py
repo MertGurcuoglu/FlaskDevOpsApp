58bbabc546f665141c9e52ce62b3325b04da56ef
MertGurcuoglu <mrthkk23@gmail.com>
2025-07-02 09:40:44 +0300

    .env dosyası eklendi

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def home():
     return '<h1>Merhaba ben Mert Gürcüoğlu ve bu bir DevOps projesidir.</h1>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
