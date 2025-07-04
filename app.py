36fa3817b459bcf80fefd24fefc8fe650d6a3761
MertGurcuoglu <mrthkk23@gmail.com>
2025-07-04 17:32:48 +0300

    app.py dekı hata duzeltıldı

--- app.py
+++ app.py
@@ -5,40 +5,42 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# test ortamı değilse DB bağlantısı kur 
+# Test ortamı değilse veritabanına bağlan
 if os.getenv("FLASK_ENV") != "testing":
-while True:
-    try:
-        conn = psycopg2.connect(
-            dbname="devdb",
-            user="devuser",
-            password="devpass",
-            host="postgres",
-            port="5432"
-        )
-        cursor = conn.cursor()
-        break  # bağlantı başarılıysa döngüden çık
-    except psycopg2.OperationalError:
-        print("PostgreSQL henüz hazır değil. 2 saniye sonra tekrar denenecek...")
-        time.sleep(2)
-
+    while True:
+        try:
+            conn = psycopg2.connect(
+                dbname="devdb",
+                user="devuser",
+                password="devpass",
+                host="postgres",
+                port="5432"
+            )
+            cursor = conn.cursor()
+            break
+        except psycopg2.OperationalError:
+            print("PostgreSQL henüz hazır değil. 2 saniye sonra tekrar denenecek...")
+            time.sleep(2)
 else:
-    conn=None
-    cursor=None        
+    conn = None
+    cursor = None
 
 @app.route('/')
 def index():
-    cursor.execute("CREATE TABLE IF NOT EXISTS visits (count INT);")
-    cursor.execute("SELECT count FROM visits;")
-    result = cursor.fetchone()
-    if result:
-        count = result[0] + 1
-        cursor.execute("UPDATE visits SET count = %s;", (count,))
+    if cursor:
+        cursor.execute("CREATE TABLE IF NOT EXISTS visits (count INT);")
+        cursor.execute("SELECT count FROM visits;")
+        result = cursor.fetchone()
+        if result:
+            count = result[0] + 1
+            cursor.execute("UPDATE visits SET count = %s;", (count,))
+        else:
+            count = 1
+            cursor.execute("INSERT INTO visits (count) VALUES (1);")
+        conn.commit()
+        return f"<h1>Bu sayfa {count} kez ziyaret edildi.</h1>"
     else:
-        count = 1
-        cursor.execute("INSERT INTO visits (count) VALUES (1);")
-    conn.commit()
-    return f"<h1>Bu sayfa {count} kez ziyaret edildi.</h1>"
+        return "<h1>Test modunda çalışıyor.</h1>"
 
 if __name__ == '__main__':
     if os.getenv("FLASK_ENV") != "testing":
--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_index():
     tester = app.test_client()
     response = tester.get('/')
     assert response.status_code == 200
-    assert "Merhaba ben Mert Gürcüoğlu" in response.data.decode("utf-8")
+    assert "test modunda çalışıyor" in response.data.decode("utf-8")
